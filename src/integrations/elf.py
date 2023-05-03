03a0670a6a614d989b61199341f4280b6c4421b3
Eugene 'marengohue' Rebedailo <marengohue@protonmail.com>
2023-05-03 21:32:14 +0200

    remove one-read-per-run

--- src/integrations/elf.py
+++ src/integrations/elf.py
@@ -1,16 +1,9 @@
 from .db import make_inventory as _make
 
-_IS_READ = False
-
 
 def read_inventory():
     """
     Returns the list of available inventory items in the form of a list of dictionaries
     """
-    global _IS_READ
 
-    if _IS_READ:
-        raise SystemError("ELF needs time to recover from the previous read.")
-
-    _IS_READ = True
     return _make()
